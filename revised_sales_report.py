44e67255b2a79f8a03f150a793284ed3f84946c0
lunaxlam@gmail.com <lunalam@Lunas-MacBook-Pro.local>
2022-04-05 18:37:39 -0700

    Refactored code

--- revised_sales_report.py
+++ revised_sales_report.py
@@ -2,31 +2,54 @@
 
 import sys
 
-# Get name of report .txt and create file object
-get_file = f'{sys.argv[1]}'
-f = open(get_file)
 
-# Initialize a dictionary of salesperson, number of melons (key, value) pairs
-sales_summary = {}
+def open_file(file):
+    """Open a file and return a file object to iterate through."""
 
-# Iterate over each line in the file object
-for line in f:
-    # Remove any whitespace to the right of each line
-    line = line.rstrip()
-    # Store each line into a list variable, using the pipe symbol as a delimiter
-    entries = line.split('|')
-    # Store the name of the salesperson by accessing the 0th index of the entries list
-    salesperson = entries[0]
-    # Store the number of elements by accessing the 2th index of the entries list and casting it as an integer type 
-    melons = int(entries[2])
+    file = open(file)
 
-    # Add salesperson and number of melons sold to the dictionary
-    if salesperson in sales_summary:
-        sales_summary[salesperson] += melons
-    else:
-        sales_summary[salesperson] = melons
+    return file
 
-# Iterate through each element in dictionary
-for salesperson, melons in sales_summary.items():
-    # Print message
-    print(f'{salesperson} sold {melons} melons.')
+
+def get_melon_sales(file):
+    """Iterate through a sales report file to return a dictionary (salesperson: number of melons) key, value pairs."""
+
+    # Initialize a dictionary of salesperson, number of melons (key, value) pairs
+    sales_summary = {}
+
+    # Iterate over each line in the file object
+    for line in file:
+        # Remove any whitespace to the right of each line
+        line = line.rstrip()
+        # Store each line into a list variable, using the pipe symbol as a delimiter
+        salesperson, total_sales, melons_sold = line.split('|')
+
+        # Add salesperson and number of melons sold to the dictionary
+        if salesperson in sales_summary:
+            sales_summary[salesperson] += int(melons_sold)
+        else:
+            sales_summary[salesperson] = int(melons_sold)
+    
+    return sales_summary
+
+
+def print_melon_report(sales_summary):
+    """Print melon sales report summary."""
+
+    # Iterate through each element in dictionary
+    for salesperson, melons_sold in sales_summary.items():
+        # Print message
+        print(f'{salesperson} sold {melons_sold} melons.')
+
+
+# Get a file as a string from the command line
+f = sys.argv[1]
+
+# Open the file
+file = open_file(f)
+
+# Generate sales info
+sales_summary = get_melon_sales(file)
+
+# Print message
+print(print_melon_report(sales_summary))
